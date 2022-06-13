ed5dc545f74ca66eddbd01c3e7a4d12d1fbfc521
Tibor Gazsovics <gazsovicst@gmail.com>
2022-06-13 18:52:33 +0100

    sixteenth challenge

---
+++ nth_child_array.js
@@ -0,0 +1,7 @@
+// Write a function that takes an array (a) and a value (n) as argument
+// Return the nth element of 'a'
+function myFunction(a, n) {
+    return a[n-1];
+}
+
+console.log(myFunction([10,9,8,7,6],5));
